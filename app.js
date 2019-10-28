7b9e900ab16315b9a95ecf97b35e411274c9ab99
samking24 <samdeveloper24x7@gmail.com>
2019-10-28 14:36:12 +0530

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const globalErrorHandler = require('./controller/errorController');
 const AppError = require('./utils/appError');
@@ -63,6 +64,9 @@ app.use(
   })
 );
 
+app.use(cors());
+
+app.options('*', cors());
 //Serving static File
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
