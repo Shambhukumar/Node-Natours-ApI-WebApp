4c58a8692a1a1e64081bb25163f76abf5e84d1e7
samking24 <samdeveloper24x7@gmail.com>
2019-10-29 10:00:46 +0530

    Added Sign Up

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,7 @@ export const logout = async () => {
       url: '/api/v1/users/logout'
     });
     if ((res.data.status = 'success')) {
-      location.reload(true);
+      location.assign('/');
       showAlert('success', 'Logged Out Successfully');
     }
   } catch (err) {
